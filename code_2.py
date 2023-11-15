35cf844d76023bd61e227003f65f96a26583f107
Uroma002 <146116449+uroma002@users.noreply.github.com>
2023-11-15 08:47:33 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,13 +1,30 @@
-def generate_random_data():
+    const data = generateRandomData();
 
-    main()
-    data = generate_random_data()
-import random
-    for item in data:
-        print(f"Random Number: {item}")
+    });
+        data.push(Math.floor(Math.random() * 100) + 1);
+    }
+        console.log("Random Number: " + item);
+    const data = [];
+    return data;
 
+main();
+    for (let i = 0; i < 10; i++) {
+}
+
+    data.forEach(item => {
+function main() {
+}
+function generateRandomData() {import random
 if __name__ == "__main__":
-    return data
 def main():
 
+    return data
+def generate_random_data():
+
     data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+    data = generate_random_data()
+
+        print(f"Random Number: {item}")
+
+    main()
